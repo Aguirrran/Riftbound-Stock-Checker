6b64c8a1b463e039fbd8f7a73cc059f3f231e0e8
Christian Aguirre <68363708+Aguirrran@users.noreply.github.com>
2026-06-17 05:38:58 -0500

    Complete basic console product tracker

--- main.cpp
+++ main.cpp
@@ -1,23 +1,43 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 //To compile you must do g++ main.cpp -o main
-int main(){
+struct Product{ 
     string productName = "";
     string storeName = "";
     string productLink = "";
     string status = "";
+};
+int main(){
+    Product product;
+    vector<Product> products; 
+    while(true){
     cout << "Enter, your product name: \n";
-    getline(cin,productName);
+    getline(cin,product.productName);
     cout << "Enter, your store name: \n";
-    getline(cin,storeName);
+    getline(cin,product.storeName);
     cout << "Enter, your product link: \n";
-    getline(cin,productLink);
+    getline(cin,product.productLink);
     cout << "current status?: \n";
-    getline(cin,status);
-    cout << "Product Name: " << productName << endl;
-    cout << "Store Name: " << storeName << endl;
-    cout << "Product Link: " << productLink << endl;
-    cout << "Current Status: " << status << endl;
+    getline(cin,product.status);
+    products.push_back(product);
+    cout << "Would you like to add another product?\n";
+    string temp;
+    getline(cin,temp);
+        if(temp == "No" || temp == "N" || temp == "n" || temp == "no"){
+            break;
+        }
+    }
+    cout << "printing.. current products\n";
+    for (size_t i = 0; i < products.size(); i++){
+    cout << "Product " << i+1 << endl;
+    cout << "Product Name: " << products[i].productName << "\n";
+    cout << "Store Name: " << products[i].storeName << "\n";
+    cout << "Product Link: " << products[i].productLink << "\n";
+    cout << "Status: " << products[i].status << "\n";
+    cout << endl;
+    }
 }
+
